36c79abb814342f0e392d1399371c1622388b7e9
Théo TRUVELOT <theotruvelott@gmail.com>
2024-12-23 10:40:01 +0100

    learn slice and for

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
-
 func main() {
 	//Plusieurs façon de déclarer des variables
 	//var card string = "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
+	//card := "Ace of Spades"
+	//card := newCard()
+	//fmt.Println(card)
+
+	////création d'une slice
+	//cards := []string{"Ace of Diamonds", newCard()}
+	////ajout dans un slice
+	//cards = append(cards, "Six of Spades")
+	////boucle
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+
 }
 
 // Type of return = string
